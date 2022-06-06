f408fbabbb68f5cc111b89ed408a49545cd943f9
Funnym0th <52089905+funnym0th@users.noreply.github.com>
2022-06-06 19:54:33 -0400

    Delete eq 2 completas.py

--- eq 2 completas.py
+++
@@ -1,9 +0,0 @@
-import math
-valorA = int(input("Inserta el valor A: "))
-valorB = int(input("Inserta el valor B: "))
-valorC = int(input("Inserta el valor C: "))
-
-raiz = math.sqrt(valorB * valorB - 4 * valorA * valorC)
-divisor = 2 * valorA
-print("El valor X1 de la ecuación es: ",(-valorB + raiz) / divisor)
-print("El valor X2 de la ecuación es: ",(-valorB - raiz) / divisor)
