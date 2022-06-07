9672476e25d09b69b28e9e16fe4767cf17fec604
Funnym0th <52089905+funnym0th@users.noreply.github.com>
2022-06-06 23:29:38 -0400

    Delete cuadraticas-C=0.py

--- matematica/cuadraticas-C=0.py
+++
@@ -1,7 +0,0 @@
-import math
-
-valorA = int(input("Inserta el valor A: "))
-valorB = int(input("Inserta el valor B: "))
-raiz = math.sqrt(valorB * valorB)
-print("El valor de X1 es igual a: ",(-valorB + raiz) / (2 * valorA))
-print("El valor de X2 es igual a: ",(-valorB - raiz) / (2 * valorA))
