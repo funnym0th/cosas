731d5121dc198bad8e05beea2108ee6c53bdeb07
Camilo <52089905+funnym0th@users.noreply.github.com>
2024-04-10 23:30:00 -0400

    Update quadratic.py

--- matematica/quadratic.py
+++ matematica/quadratic.py
@@ -1,19 +1,40 @@
 import math
 
-def quadraticFunction(exponentValue, xValue, naturalValue):
-    if((exponentValue, xValue, naturalValue) != 0):
-        quadraticSqrt = math.sqrt(xValue ** 2 - 4 * exponentValue * naturalValue)
-        firstResult = (-xValue + quadraticSqrt) / 2 * exponentValue
-        secondResult = (-xValue - quadraticSqrt) / 2 * exponentValue
-        return firstResult, secondResult, "Type: All values"
-    elif(xValue == 0, (exponentValue, naturalValue) != 0):
-        quadraticSqrt = math.sqrt(- 4 * exponentValue * naturalValue)
-        firstResult = (quadraticSqrt) / 2 * exponentValue
-        secondResult = (-quadraticSqrt) / 2 * exponentValue
-        return firstResult, secondResult, "Type: No bX value"
-    elif(naturalValue == 0, (exponentValue, xValue) != 0):
-        firstResult = (-xValue + quadraticSqrt) / 2 * exponentValue
-        secondResult = (-xValue - quadraticSqrt) / 2 * exponentValue
-        return firstResult, secondResult, "Type: No natural value"
+def quadraticFunction(exponentValue, crossValue, naturalValue, result, isResultCross):
+    if((exponentValue, crossValue, naturalValue) != 0):
+        quadraticSqrt = math.sqrt(crossValue ** 2 - 4 * exponentValue * naturalValue)
+        firstResult = (-crossValue + quadraticSqrt) / (2 * exponentValue)
+        secondResult = (-crossValue - quadraticSqrt) / (2 * exponentValue)
+        return "First coordinate:", firstResult, "Second coordinate:", secondResult, "Type: All values"
+    #EL SCRIPT SIN VALOR BX NO ESTA PREPARADO PARA RECIBIR UN VALOR BX DEL OTRO LADO DEL SIMBOLO IGUAL !!
+    elif(crossValue == 0, (exponentValue, naturalValue) != 0):
+        if(result != 0 and isResultCross == False):
+            naturalValue = naturalValue -(result)
+            quadraticSqrt = math.sqrt(- 4 * exponentValue * naturalValue)
+            firstResult = quadraticSqrt / (2 * exponentValue)
+            secondResult = -quadraticSqrt / (2 * exponentValue)
+            return "First coordinate:", firstResult, "Second coordinate:", secondResult, "Type: No bX value, natural value imported from right side"
+        else:
+            quadraticSqrt = math.sqrt(- 4 * exponentValue * naturalValue)
+            firstResult = quadraticSqrt / (2 * exponentValue)
+            secondResult = -quadraticSqrt / (2 * exponentValue)
+            return "First coordinate:", firstResult, "Second coordinate:", secondResult, "Type: No bX value"
+    elif(naturalValue == 0, (exponentValue, crossValue) != 0):
+        if(result != 0 and isResultCross == True):
+            crossValue = crossValue -(result)
+            quadraticSqrt = math.sqrt(- 4 * exponentValue * naturalValue)
+            firstResult = (-crossValue + quadraticSqrt) / (2 * exponentValue)
+            secondResult = (-crossValue - quadraticSqrt) / (2 * exponentValue)
+            return "First coordinate:", firstResult, "Second coordinate:", secondResult, "Type: No natural value, BX value from right side imported to left side"
+        elif(result != 0 and isResultCross== False):
+            naturalValue == -(result)
+            quadraticSqrt = math.sqrt(crossValue ** 2 - 4 * exponentValue * naturalValue)
+            firstResult = (-crossValue + quadraticSqrt) / (2 * exponentValue)
+            secondResult = (-crossValue - quadraticSqrt) / (2 * exponentValue)
+            return "First coordinate:", firstResult, "Second coordinate:", secondResult, "Type: Natural value imported from right side to left side"
+        else:
+            firstResult = (-crossValue + quadraticSqrt) / (2 * exponentValue)
+            secondResult = (-crossValue - quadraticSqrt) / (2 * exponentValue)
+            return "First coordinate:", firstResult, "Second coordinate:", secondResult, "Type: No natural value"
 
 print(quadraticFunction(int(input()), int(input()), int(input())))
